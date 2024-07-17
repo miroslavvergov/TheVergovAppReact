b39b6391550fb9e6499786c44df761ce8ed91d6f
miroslavvergov <gabberlt123@gmail.com>
2024-07-17 19:49:37 +0300

    Route configuration

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { setupStore } from './store/store.ts';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import Login from './components/Login.tsx';
+import { Provider } from 'react-redux';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const store = setupStore();
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<App />}>
+    <Route path='login' element={<Login />} />
+  </Route>
+))
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+root.render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+);
